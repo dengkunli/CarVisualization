c895ae40607b5d0a5f1f0442e6ef0c1751facd09
Kunli Deng <dengkunli@foxmail.com>
2016-12-06 11:02:27 +0100

    Added csv original data, and a script to convert csv to json, and the converted json

---
+++ data/csvtojson.js
@@ -0,0 +1,39 @@
+const [
+    fs,
+    readline
+]
+    = [
+    require('fs'),
+    require('readline')
+];
+
+const src = fs.createReadStream('cars.csv');
+const dest = fs.createWriteStream('cars.json');
+
+const reader = readline.createInterface({
+    input: src
+});
+
+const data = [];
+let isHeader = true;
+let headers;
+
+reader.on('line', (line) => {
+    if (isHeader) {
+        headers = line.split(';');
+        isHeader = false;
+    }
+    else {
+        data.push(line.split(';').reduce((obj, val, ind) => {
+            obj[headers[ind]] = val;
+            return obj;
+        }, {}));
+    }
+});
+
+reader.on('close', () => {
+    // console.log('close', JSON.stringify(data));
+    dest.write(JSON.stringify(data));
+});
+
+
